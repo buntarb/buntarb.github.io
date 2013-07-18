f80f88a6a14ba79eaed074dde606a3e0f95fd755
buntarb <buntarb@gmail.com>
2013-07-18 13:22:43 +0300

    Update zz-application.js

--- libs/client/scripts/zz-application.js
+++ libs/client/scripts/zz-application.js
@@ -1,1 +1,7 @@
-tmp
+angular.module('zz', []).
+  config(['$routeProvider', function($routeProvider){
+    $routeProvider.
+      when('/app/', {templateUrl: 'partials/phone-list.html',   controller: PhoneListCtrl}).
+      when('/phones/:phoneId', {templateUrl: 'partials/phone-detail.html', controller: PhoneDetailCtrl}).
+      otherwise({redirectTo: '/phones'});
+  }]);
